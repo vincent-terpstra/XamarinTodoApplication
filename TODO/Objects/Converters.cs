2a36047c0e1a3bc75222de60096c80c2a5c3f9aa
Vincent Terpstra <vdterpst@gmail.com>
2022-05-05 22:49:44 -0400

    Added Converter to format date in all project view

---
+++ TODO/Objects/Converters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace TODO.Objects;
+
+public class DateTimeToString : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return ((DateTime) value).ToString("MMM dd, yyyy");
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
