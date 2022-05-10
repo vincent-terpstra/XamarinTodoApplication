79928ca762e533328700948c237b85bc475a999f
Vincent Terpstra <vdterpst@gmail.com>
2022-05-09 21:20:23 -0400

    Added lightmode darkmode, ways to switch theme

--- TODO/AppShell.xaml.cs
+++ TODO/AppShell.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TODO.Themes;
 using TODO.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -18,5 +19,24 @@ public partial class AppShell : Shell
         Routing.RegisterRoute(nameof(EditTaskView),typeof(EditTaskView));
         Routing.RegisterRoute(nameof(ProjectView),typeof(ProjectView));
         Routing.RegisterRoute(nameof(EditProjectView),typeof(EditProjectView));
+        
+        setTheme(Application.Current.RequestedTheme);
+    }
+
+    private void setTheme(OSAppTheme theme)
+    {
+        ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+        if(mergedDictionaries == null)
+            return;
+        mergedDictionaries.Clear();
+        Application.Current.UserAppTheme = theme;
+        mergedDictionaries.Add(theme == OSAppTheme.Dark ? new DarkTheme() : new LightTheme());
+        
+    }
+    
+    private void OnSwitchTheme(object sender, EventArgs e)
+    {
+            OSAppTheme currentTheme = Application.Current.UserAppTheme;
+            setTheme(currentTheme == OSAppTheme.Dark ? OSAppTheme.Light : OSAppTheme.Dark);
     }
 }
---
+++ TODO/Themes/DarkTheme.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TODO.Themes;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class DarkTheme : ResourceDictionary
+{
+    public DarkTheme()
+    {
+        InitializeComponent();
+    }
+}
